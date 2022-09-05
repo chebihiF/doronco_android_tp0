0d588c111f3a224632a219e3bc1e636d8e210ae9
chebihiF <chebihi.f@ucd.ac.ma>
2022-09-05 16:00:26 +0100

    add event handler

--- app/src/main/java/org/doronco/tp0/MainActivity.java
+++ app/src/main/java/org/doronco/tp0/MainActivity.java
@@ -3,14 +3,30 @@ package org.doronco.tp0;
         import androidx.appcompat.app.AppCompatActivity;
 
         import android.os.Bundle;
+        import android.service.autofill.OnClickAction;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        button = findViewById(R.id.btn1);
+        button.setText("Valider");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Hello world",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
+    public void showMessage(View view) {
+        Toast.makeText(this,"Hello world",Toast.LENGTH_LONG).show();
+    }
 }
